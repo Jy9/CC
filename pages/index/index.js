6f8083eb9f0bc313f984bb357a3f1984e8eb2b22
jiayue <jayue@163.com>
2018-03-13 18:24:57 +0800

    cc

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,24 +4,54 @@ Page({
     isCard:"none",
     cardOP:0,
     cardBottom:"none",
-    footerOP:"0.3",
+    footerBd:0.3,
+    footer:{
+    	footerOpacity:0,
+    	footerWidth:"0rpx",
+    	footerHeight:"0rpx",
+    	footerShow:"none"
+    },
     compile: "none",//inling-block
-    sexs: [
-        "https://raw.githubusercontent.com/Jy9/icon/master/woman.png",
-        "https://raw.githubusercontent.com/Jy9/icon/master/man.png"
-    ],
+    iconUrl:"https://raw.githubusercontent.com/Jy9/icon/master/",
+    sexs: ["woman.png","man.png"],
     user:null
   },
   onLoad: function () {
-      this.setData({
+    var thisData = this;
+    thisData.setData({
           user:{
               "name": "贾越",
               "sex": 0,
-              "image":"../icon/dog.png",
+              "image":"https://raw.githubusercontent.com/Jy9/icon/master/portrait.png",
               "birthday": "1994/12/17",
               "address": "海淀区 朱芳后坟地",
+              "register":"榆林市 吴堡县",
               "signature": "生活不只眼前的苟且，还有明天的枸杞"
           }
+      })
+    thisData.setData({
+        content:[{
+          date:"2108/3/13",
+          title:"我是开发者",
+          name:"贾越",
+          details:"奥萨蒂哦我的阿斯觉得我啊为基地",
+          image:"portrait.png",
+          praise:112
+        }, {
+          date: "2108/3/13",
+          title: "我是开发者",
+          name: "贾越",
+          details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+          image: "portrait.png",
+          praise: 112
+          }, {
+            date: "2108/3/13",
+            title: "我是开发者",
+            name: "贾越",
+            details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+            image: "portrait.png",
+            praise: 112
+          }]
       })
       /*wx.login({
           success: function (res) {
@@ -39,9 +69,8 @@ Page({
     var obj = this;
     obj.setData({
       cardOP:1,
-      compileicon: "../icon/compile.png",
       compile: "none",
-      footerOP:"0"
+      footerBd:0
     });
     setTimeout(function(){
       obj.setData({
@@ -49,6 +78,35 @@ Page({
         cardBottom: "mycardshow 1s"
       });
     },400)
+  },
+  footertouchmove:function(){
+    console.log("o")
+  	this.setData({
+      footerBd:0,
+      footer:{
+	   		footerOpacity:1,
+	    	footerWidth:"360rpx",
+	    	footerHeight:"270rpx",
+	    	footerShow:"myfootershow 0.6s"
+	   	}
+   });
+  },
+  footertouchend:function(){
+    console.log("p")
+    var thisData = this;
+    thisData.setData({
+      footer:{
+	   		footerOpacity:0,
+	    	footerWidth:"0rpx",
+	    	footerHeight:"0rpx",
+	    	footerShow:"myfooterhide 1s"
+	   	}
+   });
+    setTimeout(function () {
+      thisData.setData({
+        footerBd: 0.3
+      });
+    }, 1000)
   },
   cardBtn:function(){
     var obj = this;
@@ -58,7 +116,7 @@ Page({
     setTimeout(function () {
       obj.setData({
         isCard: "none",
-        footerOP:"0.3"
+        footerBd:0.3
       });
     },400)
   },
