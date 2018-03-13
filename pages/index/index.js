3be3b6e33555079ca33b981749284ab80ad2b129
jiayue999 <m15601215226@163.com>
2018-03-13 23:12:29 +0800

    d

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,45 +3,82 @@ Page({
   data: {
       peoplenum:12,
     iconUrl:"https://raw.githubusercontent.com/Jy9/icon/master/",
-    sexs: ["woman.png","man.png"],
-    user:null
+    sexs: ["woman.png","man.png"]
   },
   onLoad: function () {
     var thisData = this;
-    thisData.setData({
-          user:{
-              "name": "贾越",
-              "sex": 0,
-              "image":"https://raw.githubusercontent.com/Jy9/icon/master/portrait.png",
-              "birthday": "1994/12/17",
-              "address": "海淀区 朱芳后坟地",
-              "register":"榆林市 吴堡县",
-              "signature": "生活不只眼前的苟且，还有明天的枸杞"
-          }
-      })
     thisData.setData({
         content:[{
           date:"2108/3/13",
           title:"我是开发者",
           name:"贾越",
           details:"奥萨蒂哦我的阿斯觉得我啊asd阿d阿三大王的啊是的哇打算的阿三大王的asd撒旦为基地",
-          image:"portrait.png",
+          image:"photo.jpg",
           praise:112
         }, {
           date: "2108/3/13",
           title: "我是开发者",
           name: "贾越",
           details: "奥萨蒂哦我的阿斯觉得我啊为基地",
-          image: "portrait.png",
+          image: "photo1.jpg",
           praise: 112
-          }, {
+            }, {
+                date: "2108/3/13",
+                title: "我是开发者",
+                name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+                image: "photo1.jpg",
+                praise: 112
+        }, {
             date: "2108/3/13",
             title: "我是开发者",
             name: "贾越",
             details: "奥萨蒂哦我的阿斯觉得我啊为基地",
-            image: "portrait.png",
+            image: "photo1.jpg",
             praise: 112
-          }]
+            }, {
+                date: "2108/3/13",
+                title: "我是开发者",
+                name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+                image: "photo1.jpg",
+                praise: 112
+        }, {
+            date: "2108/3/13",
+            title: "我是开发者",
+            name: "贾越",
+            details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+            image: "photo1.jpg",
+            praise: 112
+            }, {
+                date: "2108/3/13",
+                title: "我是开发者",
+                name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+                image: "photo1.jpg",
+                praise: 112
+        }, {
+            date: "2108/3/13",
+            title: "我是开发者",
+            name: "贾越",
+            details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+            image: "photo1.jpg",
+            praise: 112
+            }, {
+                date: "2108/3/13",
+                title: "我是开发者",
+                name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+                image: "photo1.jpg",
+                praise: 112
+        }, {
+            date: "2108/3/13",
+            title: "我是开发者",
+            name: "贾越",
+            details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+            image: "photo1.jpg",
+            praise: 112
+        }]
       })
       /*wx.login({
           success: function (res) {
