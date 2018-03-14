b1c11086cb7cd082852b5601551f877e7fc8915c
jiayue999 <m15601215226@163.com>
2018-03-14 23:43:15 +0800

    cc

--- pages/personalcenter/personalcenter.js
+++ pages/personalcenter/personalcenter.js
@@ -1,45 +1,92 @@
 //personalcenter.js
 Page({
-  data: {
-    iconUrl: "https://raw.githubusercontent.com/Jy9/icon/master/",
-    navclass:{
-      nav1class:"active",
-      nav2class:"none",
-      nav3class:"none"
+    data: {
+        iconUrl: "https://raw.githubusercontent.com/Jy9/icon/master/",
+        headerOP: 0,
+        unfold: {
+            height: "220rpx",
+            cont: "(っ´Ι`)っ"
+        },
+        navclass: {
+            nav1class: "active",
+            nav2class: "none",
+            content1class: "active",
+            content2class: "none"
+        },
+        content1: [],
+        content2: []
+    },
+    onLoad: function () {
+        var thisData = this;
+        thisData.setData({
+            content1: [{
+                date: "2108/3/13",
+                title: "我是开发者",
+                name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊asd阿d阿三大王的啊是的哇打算的阿三大王的asd撒旦为基地",
+                image: "photo.jpg",
+                praise: 112
+            }, {
+                date: "2108/3/13",
+                title: "我是开发者",
+                name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+                image: "photo1.jpg",
+                praise: 112
+            }],
+            content2: [{
+                date: "2108/3/13",
+                title: "我是开发者",
+                name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊asd阿d阿三大王的啊是的哇打算的阿三大王的asd撒旦为基地",
+                image: "photo.jpg",
+                praise: 112
+            }]
+        })
+    },
+    header_back: function () {
+        wx.navigateBack({})
+    },
+    openheader: function () {
+        var thisObj = this;
+        if (thisObj.data.unfold.height == "220rpx") {
+            thisObj.setData({
+                headerOP: 1,
+                unfold: {
+                    height: "580rpx",
+                    cont: "(ಥ _ ಥ)"
+                }
+            })
+        } else {
+            thisObj.setData({
+                headerOP: 0,
+                unfold: {
+                    height: "220rpx",
+                    cont: "(っ´Ι`)っ"
+                }
+            })
+        }
+    },
+    nav1: function () {
+        var thisObj = this;
+        thisObj.setData({
+            navclass: {
+                nav1class: "active",
+                nav2class: "none",
+                content1class: "active",
+                content2class: "none"
+            }
+        });
+    },
+    nav2: function () {
+        var thisObj = this;
+        thisObj.setData({
+            navclass: {
+                nav1class: "none",
+                nav2class: "active",
+                content1class: "none",
+                content2class: "active"
+            }
+        });
     }
-  },
-  onLoad: function () {
-    var thisData = this;
-    
-  },
-  header_back:function(){
-    wx.navigateBack({})
-  },
-  nav1:function(){
-    this.setData({
-      navclass: {
-        nav1class: "active",
-        nav2class: "none",
-        nav3class: "none"
-      }
-    })
-  },
-  nav2: function () {
-    this.setData({
-      navclass: {
-        nav1class: "none",
-        nav2class: "active",
-        nav3class: "none"
-      }
-    })
-  },
-  nav3: function () {
-    this.setData({
-      navclass: {
-        nav1class: "none",
-        nav2class: "none",
-        nav3class: "active"
-      }
-    })
-  },
 })
