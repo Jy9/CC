7efc17541bd7620c2dc125c998897d9284d98868
jiayue999 <m15601215226@163.com>
2018-03-15 00:03:48 +0800

    cc

--- pages/personalcenter/personalcenter.js
+++ pages/personalcenter/personalcenter.js
@@ -38,6 +38,13 @@ Page({
                 date: "2108/3/13",
                 title: "我是开发者",
                 name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+                image: "photo1.jpg",
+                praise: 112
+            },{
+                date: "2108/3/13",
+                title: "我是开发者",
+                name: "贾越",
                 details: "奥萨蒂哦我的阿斯觉得我啊asd阿d阿三大王的啊是的哇打算的阿三大王的asd撒旦为基地",
                 image: "photo.jpg",
                 praise: 112
