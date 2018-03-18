9558ce0c3f4a9cbcee89ffa734b0093a8cfecf52
jiayue999 <m15601215226@163.com>
2018-03-18 16:31:49 +0800

    cc

--- pages/personalcenter/personalcenter.js
+++ pages/personalcenter/personalcenter.js
@@ -12,22 +12,46 @@ Page({
             nav1class: "active",
             nav2class: "none",
             nav3class: "none",
+            nav4class: "none",
             content1class: "active",
             content2class: "none",
-            content3class: "none"
+            content3class: "none",
+            content4class: "none"
         },
         contH:"0px",
         cont1H:"0px",
-        cont3H:"0px",
+        cont3H: "0px",
+        cont4H:"0px",
         content1: [],
         content2: [],
-        content3: []
+        content3: [],
+        content4: []
     },
     onLoad: function () {
         var thisData = this;
 
-        thisData.setData({
+        /**/thisData.setData({
             utype:"manage"
+        })
+
+        thisData.setData({
+            users: [
+                {
+                    uid: 1,
+                    name: "贾越",
+                    birthday: "1994/12/17",
+                    image: "photo.jpg",
+                    address: "海淀区 朱芳路",
+                    register: "榆林市 吴堡县"
+                }, {
+                    uid: 2,
+                    name: "贾越",
+                    birthday: "1994/12/17",
+                    image: "photo1.jpg",
+                    address: "海淀区 朱芳路",
+                    register: "榆林市 吴堡县"
+                }
+            ]
         })
 
         thisData.setData({
@@ -61,7 +85,15 @@ Page({
                 details: "奥萨蒂哦我的阿斯觉得我啊为基地",
                 image: "photo1.jpg",
                 praise: 112
-            }],
+                }],
+            content4: [{
+                date: "2108/3/13",
+                title: "我是开发者3",
+                name: "贾越",
+                details: "奥萨蒂哦我的阿斯觉得我啊为基地",
+                image: "photo1.jpg",
+                praise: 112
+            }]
         });
         setTimeout(function(){
             var query1 = wx.createSelectorQuery();
@@ -72,17 +104,22 @@ Page({
                         contH: res[0].height + "px"
                     })
             })
+            var query3 = wx.createSelectorQuery();
+            query3.select("#content3").boundingClientRect();
+            query3.exec(function (res) {
+                thisData.setData({
+                    cont3H: res[0].height + "px"
+                })
+            })
             if (thisData.data.utype == "manage") {
-                var query3 = wx.createSelectorQuery();
-                query3.select("#content3").boundingClientRect();
-                query3.exec(function (res) {
+                var query4 = wx.createSelectorQuery();
+                query4.select("#content4").boundingClientRect();
+                query4.exec(function (res) {
                     thisData.setData({
-                        cont3H: res[0].height + "px"
+                        cont4H: res[0].height + "px"
                     })
                 })
             }
-            
-            
         },400)
     },
     header_back: function () {
@@ -111,6 +148,14 @@ Page({
     publish:function(){
       console.log("发表文章")
     },
+    set:function(){
+        wx.showToast({
+            title: '暂时没有编辑功能',
+        })
+        /*wx.navigateTo({
+            url: '',
+        })*/
+    },
     nav1: function () {
         var thisObj = this;
         thisObj.setData({
@@ -119,9 +164,11 @@ Page({
                 nav1class: "active",
                 nav2class: "none",
                 nav3class: "none",
+                nav4class: "none",
                 content1class: "active",
                 content2class: "none",
-                content3class: "none"
+                content3class: "none",
+                content4class: "none"
             }
         });
     },
@@ -133,9 +180,11 @@ Page({
                 nav1class: "none",
                 nav2class: "active",
                 nav3class: "none",
+                nav4class: "none",
                 content1class: "none",
                 content2class: "active",
-                content3class: "none"
+                content3class: "none",
+                content4class: "none"
             }
         });
     },
@@ -147,9 +196,27 @@ Page({
                 nav1class: "none",
                 nav2class: "none",
                 nav3class: "active",
+                nav4class: "none",
                 content1class: "none",
                 content2class: "none",
-                content3class: "active"
+                content3class: "active",
+                content4class: "none"
+            }
+        });
+    },
+    nav4: function () {
+        var thisObj = this;
+        thisObj.setData({
+            contH: thisObj.data.cont4H,
+            navclass: {
+                nav1class: "none",
+                nav2class: "none",
+                nav3class: "none",
+                nav4class: "active",
+                content1class: "none",
+                content2class: "none",
+                content3class: "none",
+                content4class: "active"
             }
         });
     }
