602d7c624945dffc8bbb590274b3315bae7f980e
Jy9 <15601215226@163.com>
2018-04-03 23:50:53 +0800

    cc

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ App({
     peoplenum:0,
     //URL: "http://41092527.nat123.cc/",
     //iconUrl: "http://41092527.nat123.cc/icon/",
-    URL: "http://localhost:3000/",
-    iconUrl: "http://localhost:3000/icon/",
+    //URL: "http://localhost:3000/",
+    //iconUrl: "http://localhost:3000/icon/", 
+    URL: "http://140.143.62.48:3000/",
+    iconUrl: "http://140.143.62.48:3000/icon/",
     labels: ["旅游","写作","程序员","连载","游戏","购物","故事会"]
   }
 })
